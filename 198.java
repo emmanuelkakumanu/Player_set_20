894645406be566636cb97ea2c20a89a513bc7b96
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-11-23 20:39:51 +0530

    Update 198.java

--- 198.java
+++ 198.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-import com.google.common.primitives.Ints; 
+//import com.google.common.primitives.Ints; 
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
@@ -20,8 +20,24 @@ class Ideone
 			b[i]=a[i];
 		}
 		Arrays.sort(b);
-		int min=Ints.indexOf(a,b[0]);
-		int max=Ints.indexOf(a,b[n-1]);
+		//int min=Ints.indexOf(a,b[0]);
+		//int max=Ints.indexOf(a,b[n-1]);
+		
+		int min=0,max=0;
+		for(int i=0;i<n;i++)
+		if(a[i]==b[0])
+		{
+			min=i;break;
+		}
+		
+		
+		for(int i=0;i<n;i++)
+		if(a[i]==b[n-1])
+		{
+			max=i;break;
+		}
+		
+		
 		System.out.print(max-min);
 	}
 }
