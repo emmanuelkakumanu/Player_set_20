25ff7dfb638a28de758bcf6341f0f126820b10a7
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-11-23 11:24:45 +0530

    ax+b=y

---
+++ 193.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int x=s.nextInt();
+		System.out.print((a*x)+b);
+	}
+}
